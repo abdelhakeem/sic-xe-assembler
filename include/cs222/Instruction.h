7b33245b4f750de72785f2bddba9a9fe5c5d365e
Abdelhakeem Osama <abdelhakeem.osama@hotmail.com>
2018-04-27 20:03:33 +0200

    Remove Statement and update Instruction

--- include/cs222/Instruction.h
+++ include/cs222/Instruction.h
@@ -2,6 +2,7 @@
 #define CS222_INSTRUCTION_H
 
 #include <unordered_map>
+#include <Operand.h>
 
 namespace cs222 {
     class Instruction {
@@ -13,8 +14,24 @@ namespace cs222 {
                 FORMAT_4 = 8,
                 FORMAT_3_4 = 12
             };
+            Instruction(
+                    const std::string& label,
+                    const std::string& operation,
+                    const Operand& firstOperand,
+                    const Operand& secondOperand,
+                    const std::string& comment);
+            std::string getLabel() const;
+            std::string getOperation() const;
+            Operand getFirstOperand() const;
+            Operand getSecondOperand() const;
+            std::string getComment() const;
             const static std::unordered_map<Format, size_t> Length;
         private:
+            const std::string label;
+            const std::string operation;
+            const Operand firstOperand;
+            const Operand secondOperand;
+            const std::string comment;
     };
 }
 
--- src/Instruction.cpp
+++ src/Instruction.cpp
@@ -1,6 +1,40 @@
 #include <Instruction.h>
 
 namespace cs222 {
+    Instruction::Instruction(
+            const std::string& label,
+            const std::string& operation,
+            const Operand& firstOperand,
+            const Operand& secondOperand,
+            const std::string& comment) :
+        label(label), operation(operation), firstOperand(firstOperand),
+        secondOperand(secondOperand), comment(comment) { }
+
+    std::string Instruction::getLabel() const
+    {
+        return label;
+    }
+
+    std::string Instruction::getOperation() const
+    {
+        return operation;
+    }
+
+    Operand Instruction::getFirstOperand() const
+    {
+        return firstOperand;
+    }
+
+    Operand Instruction::getSecondOperand() const
+    {
+        return secondOperand;
+    }
+
+    std::string Instruction::getComment() const
+    {
+        return comment;
+    }
+
     const std::unordered_map<Instruction::Format, size_t> Instruction::Length {
         { Instruction::FORMAT_1, 1 },
         { Instruction::FORMAT_2, 2 },
