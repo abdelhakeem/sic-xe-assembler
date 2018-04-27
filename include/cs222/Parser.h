a4eb9da449b9eaa992d73950cf58cce67e9878db
Abdelhakeem Osama <abdelhakeem.osama@hotmail.com>
2018-04-28 00:47:49 +0200

    Implement most of Parser

--- include/cs222/Parser.h
+++ include/cs222/Parser.h
@@ -3,18 +3,29 @@
 
 #include <istream>
 #include <memory>
-#include <Statement.h>
+#include <regex>
+#include <cs222/Instruction.h>
 
 namespace cs222 {
     class Parser {
         public:
             Parser(std::istream& inputStream);
             bool hasNext() const;
-            Statement next();
+            std::shared_ptr<Instruction> next();
         private:
             std::istream& inputStream;
-            bool endOfFile;
-            std::unique_ptr<Statement> lastStatement;
+            std::string line;
+            size_t lineNumber;
+            std::bitset<6> flags;
+            std::shared_ptr<Instruction> lastInstruction;
+            void throwError(const std::string& error) const;
+            std::string extractLabel() const;
+            std::string extractOperation();
+            std::pair<Operand, Operand> extractOperands();
+            std::string extractComment() const;
+            static const std::regex label_regex;
+            static const std::regex operation_regex;
+            static bool isAllSpaces(const std::string& str);
     };
 }
 
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,16 +1,159 @@
-#include <Parser.h>
+#include <algorithm>
+#include <stdexcept>
+#include <cs222/OpTable.h>
+#include <cs222/Parser.h>
 
 namespace cs222 {
-    Parser::Parser(std::istream& inputStream) : inputStream(inputStream) { }
+    Parser::Parser(std::istream& inputStream) :
+        inputStream(inputStream), lineNumber(0)
+    {
+        if (!inputStream.good())
+        {
+            throw std::runtime_error("Parser: malformed input stream");
+        }
+    }
 
     bool Parser::hasNext() const
     {
-        return !endOfFile;
+        return inputStream.good();
     }
 
-    Statement Parser::next()
+    std::shared_ptr<Instruction> Parser::next()
     {
-        // TODO parse next line from inputStream,
-        // set lastStatement, and return the object
+        if (inputStream.good())
+        {
+            flags.reset();
+            std::getline(inputStream, line);
+            lineNumber += 1;
+
+            // TODO parse comment lines
+            const std::string label = extractLabel();
+            const std::string operation = extractOperation();
+            const std::pair<Operand, Operand> operands = extractOperands();
+            const std::string comment = extractComment();
+
+            lastInstruction.reset(new Instruction(
+                        lineNumber, label, operation, operands.first,
+                        operands.second, comment, flags));
+        }
+
+        return lastInstruction;
+    }
+
+    void Parser::throwError(const std::string& error) const
+    {
+        throw std::invalid_argument(std::string("Parser: ") +
+                std::to_string(lineNumber) + ": " + error);
+    }
+
+    std::string Parser::extractLabel() const
+    {
+        constexpr auto LABEL_BEGIN = 0;
+        constexpr auto LABEL_END = 8;
+        constexpr auto LABEL_LENGTH = LABEL_END - LABEL_BEGIN;
+
+        std::string label = line.substr(LABEL_BEGIN, LABEL_LENGTH);
+        if (label.length() != LABEL_LENGTH)
+        {
+            throwError(std::string("expected ") +
+                    std::to_string(LABEL_LENGTH) +
+                    " characters for the label");
+        }
+
+        if (isAllSpaces(label))
+        {
+            return "";
+        }
+
+        std::smatch match;
+        if (!std::regex_match(label, match, label_regex))
+        {
+            throwError("illegal symbol name: " + label);
+        }
+
+        return match[1];
+    }
+
+    std::string Parser::extractOperation()
+    {
+        constexpr auto OPERATION_BEGIN = 9;
+        constexpr auto OPERATION_END = 15;
+        constexpr auto OPERATION_LENGTH = OPERATION_END - OPERATION_BEGIN;
+
+        if (!isspace(line[OPERATION_BEGIN-1]))
+        {
+            throwError("expected whitespace before operation");
+        }
+
+        std::string operation = line.substr(OPERATION_BEGIN, OPERATION_LENGTH);
+        if (operation.length() != OPERATION_LENGTH)
+        {
+            throwError(std::string("expected ") +
+                    std::to_string(OPERATION_LENGTH) +
+                    " characters for the operation");
+        }
+
+        std::smatch match;
+        if (!std::regex_match(operation, match, operation_regex))
+        {
+            throwError("illegal operation name: " + operation);
+        }
+
+        std::string prefix = match[1];
+        operation = match[2];
+
+        if (OpTable.find(operation) == OpTable.end())
+        {
+            throwError("invalid operation: " + operation);
+        }
+
+        if (prefix == "+")
+        {
+            flags.set(Instruction::FLAG_EXTENDED);
+        }
+        else if (!prefix.empty())
+        {
+            throwError("invalid operation prefix: " + prefix);
+        }
+
+        return operation;
+    }
+
+    std::pair<Operand, Operand> Parser::extractOperands()
+    {
+        // TODO implement function
+        return std::pair<Operand, Operand>();
+    }
+
+    std::string Parser::extractComment() const
+    {
+        constexpr auto COMMENT_BEGIN = 35;
+        constexpr auto COMMENT_END = 66;
+        constexpr auto COMMENT_MAX = COMMENT_END - COMMENT_BEGIN;
+
+        if (line.length() <= COMMENT_BEGIN)
+        {
+            return "";
+        }
+
+        std::string comment = line.substr(COMMENT_BEGIN);
+        if (comment.length() > COMMENT_MAX)
+        {
+            throwError(std::string("expected no more than ") +
+                    std::to_string(COMMENT_MAX) +
+                    " characters for the comment");
+        }
+
+        return comment;
+    }
+
+    const std::regex Parser::label_regex("^([A-Za-z][A-Za-z0-9]*)\\s*$");
+
+    const std::regex Parser::operation_regex(
+            "^(\\+?)([A-Za-z]+)\\s*$");
+
+    bool Parser::isAllSpaces(const std::string& str)
+    {
+        return std::all_of(str.begin(), str.end(), isspace);
     }
 }
