09a87e10f013aede1f94f00d647c0c16fccfe797
Abdelhakeem Osama <abdelhakeem.osama@hotmail.com>
2018-04-30 01:27:39 +0200

    Update ParserTest.cpp

--- test/ParserTest.cpp
+++ test/ParserTest.cpp
@@ -8,18 +8,44 @@ int main()
 {
     try
     {
-        cs222::Parser parser(std::cin);
+        const std::string OPERAND_TYPE[] {
+            "NONE",
+            "SYMBOL",
+            "ADDRESS",
+            "CHAR_LITERAL",
+            "HEX_LITERAL",
+            "CHAR_CONSTANT",
+            "HEX_CONSTANT",
+            "REGISTER",
+            "NUMBER",
+            "LOCCTR"
+        };
+
+        std::cout << "Parser Test (enter an empty line to exit)" << std::endl;
+        std::cout << "-----------------------------------------" << std::endl;
+
+        std::string line;
+        std::istringstream isstream;
+        cs222::Parser parser(isstream);
         std::shared_ptr<cs222::Instruction> current;
-        while (parser.hasNext())
+        while (getline(std::cin, line) && !line.empty())
         {
+            isstream.str(line);
+            isstream.clear();
             current = parser.next();
             std::cout << "Line: " << current->getLineNumber() << std::endl;
             std::cout << "Label: " << current->getLabel() << std::endl;
             std::cout << "Operation: " << current->getOperation()
                 << std::endl;
+            cs222::Operand firstOp = current->getFirstOperand();
+            cs222::Operand secondOp = current->getSecondOperand();
+            std::cout << "First Operand: " << OPERAND_TYPE[firstOp.getType()]
+                << ": \"" << firstOp.getValue() << "\"" << std::endl;
+            std::cout << "Second Operand: " << OPERAND_TYPE[secondOp.getType()]
+                << ": \"" << secondOp.getValue() << "\"" << std::endl;
             std::cout << "Comment: " << current->getComment() << std::endl;
             std::cout << "Flags: " << current->getFlags() << std::endl;
-            std::cout << std::endl;
+            std::cout << "-----------------------------------------" << std::endl;
         }
     }
     catch (const std::exception& e)
