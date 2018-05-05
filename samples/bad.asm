9faf92352204964435e436b2d50669fb54400342
Abdelhakeem Osama <abdelhakeem.osama@hotmail.com>
2018-05-05 17:10:49 +0200

    Add one more sample plus some testcases

---
+++ samples/bad.asm
@@ -0,0 +1,15 @@
+.
+. Set ALPHA equal to 4 * BETA + 3 * GAMMA.
+.
+PROG01   START   1000
+FIRST    LDA     #3
+         MUL     HHH
+         RMO     A,S
+         LDA     #4
+         MUL     BETA
+         ADDR    S,A
+         STA     ALPHA
+ALPHA    RESW    1
+BETA     RESW    1
+GAMMA    RESW    1
+         END     FIRST
