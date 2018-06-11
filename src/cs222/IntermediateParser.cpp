e8682c792916e634db678d116cae2f5c031a512a
Hesham Medhat <heshammedhat5@gmail.com>
2018-06-11 06:32:27 +0200

    Skip comment lines in intermediate listing file

--- src/cs222/IntermediateParser.cpp
+++ src/cs222/IntermediateParser.cpp
@@ -26,6 +26,10 @@ namespace cs222 {
             std::stringstream sstream(line);
             std::string token;
 
+            if (token[0] == '.') {// Skip comment lines.
+                return this->next();
+            }
+
             advanceToken(sstream, token);// Skipping line number.
 
             advanceToken(sstream, token);// Expecting address.
